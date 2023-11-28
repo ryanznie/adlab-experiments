5f9225252447f40e902d08cae16637a11350a1ff
ryanznie <ryanznie@gmail.com>
2023-11-27 20:21:05 -0500

    option to tag trials

--- superconductor/data-scripts/stream.py
+++ superconductor/data-scripts/stream.py
@@ -3,6 +3,8 @@ import time
 import json
 import serial
 import os
+import argparse
+from argparse import RawTextHelpFormatter
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -17,22 +19,24 @@ def convert_line_to_dict(line):
     values = line.split()
     keys = ["SC_volt", "Divd_volt", "Divd_ratio", "Therm_R", "Ref_Volt"]
 
-    # Create a dictionary by zipping the keys with the corresponding values
     data_dict = dict(zip(keys, map(float, values)))
 
     return data_dict
 
-def main():
+def main(args):
+    """
+    Sending messages to IoTHub
+    """
     time.sleep(0.1)
     try:
         client = IoTHubDeviceClient.create_from_connection_string(connection)
         while True:
             incoming_line = arduino.readline()
             incoming_line = incoming_line.decode('utf-8')
-
             data = convert_line_to_dict(incoming_line)
-            # additional data
-            data['tag'] = 'test' # future implementation: stream.py -t
+
+            # add additional fields here, if necessary
+            data['tag'] = args.tag 
 
             if data != {}:
                 message = Message(json.dumps(data))
@@ -45,6 +49,21 @@ def main():
         print("Arduino Device Stopped")
 
 if __name__ == '__main__':
-    print("STREAMING DATA TO AZURE IOT HUB")
+    """
+    Example: python stream.py -t "trial1"
+    """
+    parser = argparse.ArgumentParser('IoT-stream', formatter_class=RawTextHelpFormatter)
+    parser.add_argument(
+        "--tag",
+        "-t",
+        help = "tag for each trial (default: %(default)s)",
+        required = False,
+        default = "test"
+    )
+    
+    args = parser.parse_args()
+
+    print("STREAMING DATA TO AZURE IOT HUB, TO STOP STREAM PRESS <Ctrl+C>")
     print("SC_volt, Divd_volt, Divd_ratio, Therm_R, Ref_Volt")
-    main()
+ 
+    main(args)
